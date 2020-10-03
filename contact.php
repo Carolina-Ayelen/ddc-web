c87e6b547ab19b4b0a6cb30b2adc67411462975f
Carolina Calviño <carolinaayelencalvi@hotmail.com>
2020-10-03 01:48:57 -0300

    primer commit

--- contact.php
+++ contact.php
@@ -3,7 +3,7 @@ $field_name = $_POST['nombre'];
 $field_email = $_POST['email'];
 $field_message = $_POST['consulta'];
 
-$mail_to = 'carolinaayelencalvi@hotmail.com';
+$mail_to = 'carolinacalvino@ddcweb';
 $subject = 'Message from a site visitor '.$field_name;
 
 $body_message = 'From: '.$field_name."\n";
