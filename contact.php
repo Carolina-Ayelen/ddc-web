02e25f3d37b901fd21811042201bd45cffb8db95
Carolina Calviño <carolinaayelencalvi@hotmail.com>
2020-10-05 16:17:05 -0300

    algunos cambios

--- contact.php
+++ contact.php
@@ -1,32 +1,20 @@
 <?php
-$field_name = $_POST['nombre'];
-$field_email = $_POST['email'];
-$field_message = $_POST['consulta'];
-
-$mail_to = 'carolinacalvino@ddcweb';
-$subject = 'Message from a site visitor '.$field_name;
-
-$body_message = 'From: '.$field_name."\n";
-$body_message .= 'E-mail: '.$field_email."\n";
-$body_message .= 'Message: '.$field_message;
-
-$headers = 'From: '.$field_email."\r\n";
-$headers .= 'Reply-To: '.$field_email."\r\n";
-
-$mail_status = mail($mail_to, $subject, $body_message, $headers);
-
-if ($mail_status) { ?>
-	<script language="javascript" type="text/javascript">
-		alert('Gracias por enviar su mensaje');
-		window.location = 'contact_page.html';
-	</script>
-<?php
+$nombre = $_POST['nombre'];
+$email = $_POST['email'];
+$mensaje = $_POST['mensaje'];
+$para = 'carolinaayelencalvi@hotmail.com';
+$titulo = 'ASUNTO DEL MENSAJE';
+$header = 'From: ' . $email;
+$msjCorreo = "Nombre: $nombre\n E-Mail: $email\n Mensaje:\n $mensaje";
+  
+if ($_POST['submit']) {
+if (mail($para, $titulo, $msjCorreo, $header)) {
+echo "<script language='javascript'>
+alert('Mensaje enviado, muchas gracias.');
+window.location.href = 'http://www.ddcwewb.com.ar';
+</script>";
+} else {
+echo 'Falló el envio';
 }
-else { ?>
-	<script language="javascript" type="text/javascript">
-		alert('Mensaje no enviado. Por favor envie un email a carolinaayelencalvi@hotmail.com');
-		window.location = 'contact_page.html';
-	</script>
-<?php
 }
 ?>
